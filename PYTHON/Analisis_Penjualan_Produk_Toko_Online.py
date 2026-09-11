0e4033cf68001ee336181141548c46db83fba1f8
Samuel Tambunan <samueltambz27@gmail.com>
2026-09-11 21:12:52 +0700

    Analisis Penjualan Produk Toko Online

--- PYTHON/Analisis_Penjualan_Produk_Toko_Online.py
+++ PYTHON/Analisis_Penjualan_Produk_Toko_Online.py
@@ -6,25 +6,25 @@ penjualan = {
 }
 
 # 1. Tampilkan nama produk dan kategori
-print(f"Produk : {penjualan["produk"]}")
-print(f"Kategori : {penjualan["kategori"]}")
+print(f"Produk : {penjualan['produk']}")
+print(f"Kategori : {penjualan['kategori']}")
 
-# 2. Hitung total pendapatan    
+# 2. Hitung total pendapatan
 total_pendapatan = penjualan["harga"] * penjualan["jumlah_terjual"]
 print(f"Total Pendapatan : Rp{total_pendapatan:,.0f}")
 
-# 3. Ubah harga laptop dari 7.500.000 menjadi 7.000.000
+# 3. Ubah harga laptop
 penjualan["harga"] = 7000000
 
 # 4. Tambahkan item baru
-penjualan["jumlah_terjual"] = 4
+penjualan["stok"] = 10
 
-# 5. Karena harga sudah berubah, hitung kembali total_pendapatan
+# 5. Hitung kembali total pendapatan
 total_pendapatan = penjualan["harga"] * penjualan["jumlah_terjual"]
 print(f"Total Pendapatan : Rp{total_pendapatan:,.0f}")
 
 print()
 
-# 6. Gunakan .items() untuk melakukan looping
+# 6. Loop menggunakan .items()
 for key, value in penjualan.items():
     print(f"{key} : {value}")
